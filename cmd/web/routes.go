11827293bf97b3b09bea1e8d899e00831de16ba6
Miguel Parada <miguelparada@gmail.com>
2022-01-06 16:01:59 -0600

    Added access to static files

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -24,6 +24,7 @@ func routes(app *config.AppConfig) http.Handler {
 	// mux := pat.New()
 	// mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
 	// mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
-
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
 	return mux
 }
